f254d3a4aa0c30f415b6958ed43dbf7bb54abbda
KienDoTri <kiendotri@gmail.com>
2025-02-19 22:40:11 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cau1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var context = new StudentContext())
+            {
+                var student = new Student
+                {
+                    Name = textBox1.Text,
+                    Age = int.Parse(textBox2.Text),
+                    Class = textBox3.Text
+                };
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var context = new StudentContext())
+                {
+                    dataGridView1.DataSource = context.Students.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Đã xảy ra lỗi: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cau1
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("name=StudentDBConnectionString")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
